7803d4f0e8c4ccbb4a143057266b05338caa47de
HubertTubeline <huberttubeline@gmail.com>
2018-11-19 12:50:59 +0200

    Integrate App Center

--- ToDoApp/Activities/StartActivity.cs
+++ ToDoApp/Activities/StartActivity.cs
@@ -2,6 +2,9 @@
 using Android.OS;
 using Android.Support.V7.App;
 using ToDoApp.Presenters;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace ToDoApp.Activities
 {
@@ -13,6 +16,9 @@ namespace ToDoApp.Activities
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            AppCenter.Start("dfaba3e4-1876-4b13-b2dc-bad81b1c64f5",
+                typeof(Analytics), typeof(Crashes));
+            
             _presenter = new StartPresenter(this);
 
             StartNextActivity();
